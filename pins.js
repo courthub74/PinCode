e75069c9a3f736b8366b580e2487137cd17cd27e
Courdevelops <courdevelops@gmail.com>
2024-08-24 14:44:05 -0400

    javascript started

---
+++ pins.js
@@ -0,0 +1,9 @@
+// Query the pin inputs by mapping through them
+let pin_one = document.getElementById('pin_one');
+let pin_two = document.getElementById('pin_two');
+let pin_three = document.getElementById('pin_three');
+let pin_four = document.getElementById('pin_four');
+
+
+// If a digit is entered, move to next in line
+if (pin_one);
